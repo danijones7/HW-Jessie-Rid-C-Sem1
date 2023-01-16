993a0b52263b1e560eb020733fd20af182635274
Jessie <jessica.v.rid@gmail.com>
2023-01-17 00:04:06 +0300

    The task itself for task4 added

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,4 +1,9 @@
-﻿System.Console.WriteLine("enter number");
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+System.Console.WriteLine("enter number");
 int N = Convert.ToInt32(Console.ReadLine());
 int num = 1;
 while(num <= N)
