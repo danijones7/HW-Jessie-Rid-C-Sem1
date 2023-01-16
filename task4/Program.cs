2cabc39b2512319cf2e917efedf509cce5275181
Jessie <jessica.v.rid@gmail.com>
2023-01-17 00:00:41 +0300

    Task4 is solved

---
+++ task4/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.WriteLine("enter number");
+int N = Convert.ToInt32(Console.ReadLine());
+int num = 1;
+while(num <= N)
+{
+    if(num % 2 == 0)
+    System.Console.WriteLine(num);
+    num ++;
+}
+System.Console.WriteLine("the number is negative");
