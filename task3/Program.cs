848427ff72d0278184789a9972da0aabd6e81a90
Jessie <jessica.v.rid@gmail.com>
2023-01-16 23:44:48 +0300

    Task3 is silved

---
+++ task3/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+System.Console.WriteLine("enter number");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0)
+{ System.Console.WriteLine("Yes, the number is even"); }
+else
+{
+    System.Console.WriteLine("No, the number is odd");
+}
